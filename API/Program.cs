26dbac28856a72b7d1c1367f98c9c4aef8e6b14b
rpc28970 <rpc28970@gmail.com>
2023-01-05 09:57:02 -0500

    Created React Skeleton, Configured Cors, Axios Calls, Semantic Css

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
