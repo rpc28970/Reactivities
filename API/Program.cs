6566ee187e940be9a8a676eb256d5ec887409349
rpc28970 <rpc28970@gmail.com>
2023-01-23 15:28:59 -0500

    FB Login Complete

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+        policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000", "https://localhost:3000");
     });
 });
 builder.Services.AddControllers(opt => 
@@ -40,12 +40,12 @@ app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
 app.UseCsp(opt => opt
     .BlockAllMixedContent()
-    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "sha256-DpOoqibK/BsYhobWHnU38Pyzt5SjDZuR/mFsAiVN7kk="))
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
     .FormActions(s => s.Self())
     .FrameAncestors(s => s.Self())
-    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
-    .ScriptSources(s => s.Self())
+    .ImageSources(s => s.Self().CustomSources("blob:", "data:", "https://res.cloudinary.com", "https://platform-lookaside.fbsbx.com"))
+    .ScriptSources(s => s.Self().CustomSources("https://connect.facebook.net"))
 );
 
 if (app.Environment.IsDevelopment())
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,6 +1,7 @@
+import FacebookLogin from "@greatsumini/react-facebook-login";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
-import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button, Divider } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import LoginForm from "../users/LoginForm";
 import RegisterForm from "../users/RegisterForm";
@@ -30,6 +31,22 @@ export default observer(function HomePage() {
                     <Button onClick={() => modalStore.openModal(<RegisterForm />)} size="huge" inverted>
                         Register
                     </Button>
+                    <Divider horizontal inverted>Or</Divider>
+                    <Button
+                    as={FacebookLogin}
+                    appId='583095763174154'
+                    size='huge'
+                    inverted
+                    color='facebook'
+                    content='Login with Facebook'
+                    loading={userStore.fbLoading}
+                    onSuccess={(response: any) => {
+                        userStore.facebookLogin(response.accessToken);
+                    }}
+                    onFail={(response: any) => {
+                        console.log('login failed', response);
+                    }}
+                    />
                     </>  
                 )}
             </Container>
